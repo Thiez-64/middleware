b037ff51137224604209778ee81d357155a6f712
Thiez-64 <ma.ojeanson@gmail.com>
2021-06-14 12:19:29 +0200

    The quest is finished

--- app.js
+++ app.js
@@ -1,11 +1,38 @@
 const express = require("express");
-const dotenv = require("dotenv");
+const cors = require("cors");
+require("dotenv").config();
 const app = express();
 
 app.use(express.json());
 
+const middleware = [];
+
+const md = (req, res, next) => {
+  console.log("New middleware");
+  res.on("finish", () => {
+    console.log(`res sent !`);
+  });
+  next();
+};
+
+app.use(cors());
+
 app.get("/", (req, res) => {
   res.status(200).json({ message: "hello-world" });
+});
+
+app.get("/middleware", (req, res) => {
+  res.status(200).json(middleware);
+});
+
+app.post("/api/middleware", md, (req, res) => {
+  middleware.push(req.body);
+  res.status(422).json(req.body);
+  console.log("ROute post is on");
+});
+
+app.put("/api/middleware/:id", (req, res) => {
+  res.status(200).send("Middleware has been updated");
 });
 
 const port = process.env.PORT || 4000;
